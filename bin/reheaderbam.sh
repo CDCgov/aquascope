0df28023fad4d17fbc6819462afc38217139d2e0
tzx6 <tzx6@cdc.gov>
2024-04-15 10:20:35 -0400

    Changed reheader bam for ion-torrent bams to MN908947.3 to be consistent

--- bin/reheaderbam.sh
+++ bin/reheaderbam.sh
@@ -8,13 +8,13 @@ bamheader=$(samtools view -H $bam)
 if grep -q "SO:coordinate" <<< ${bamheader}; then
     echo "BAM file is already sorted"
     # Reheader the sorted BAM file
-    samtools view -H "$bam" | awk -v OFS='\t' '{ if ($1 == "@SQ" && $2 == "SN:2019-nCoV") $2 = "SN:NC_045512.2"; print }' | samtools reheader -P - $bam > "${bam}_reheadered.bam"
+    samtools view -H "$bam" | awk -v OFS='\t' '{ if ($1 == "@SQ" && $2 == "SN:2019-nCoV") $2 = "SN:MN908947.3"; print }' | samtools reheader -P - $bam > "${bam}_reheadered.bam"
 else
     echo "BAM file is not sorted, sorting now"
     # Sort the unsorted BAM file
     samtools sort -o "${bam}_sorted.bam" -T "${bam}_temp" "$bam"
     # Reheader the sorted BAM file
-    samtools view -H "${bam}_sorted.bam" | awk -v OFS='\t' '{ if ($1 == "@SQ" && $2 == "SN:2019-nCoV") $2 = "SN:NC_045512.2"; print }' | samtools reheader -P - $bam > "${bam}_reheadered.bam"
+    samtools view -H "${bam}_sorted.bam" | awk -v OFS='\t' '{ if ($1 == "@SQ" && $2 == "SN:2019-nCoV") $2 = "SN:MN908947.3"; print }' | samtools reheader -P - $bam > "${bam}_reheadered.bam"
     rm "${bam}_sorted.bam"
 fi
 
