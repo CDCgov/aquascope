bc649940bd8ec8e22efd060150c7c28d2391ea9f
tzx6 <tzx6@cdc.gov>
2024-06-13 23:23:32 -0400

    Added indexing to reheader.bam

--- bin/reheaderbam.sh
+++ bin/reheaderbam.sh
@@ -13,6 +13,8 @@ SNID=$(awk 'NR>5 {print $1; exit}' "$gff")
 # Reheader the BAM file
 samtools view -H "$bam" | awk -v OFS='\t' -v SNID="$SNID" '{ if ($1 == "@SQ" && sub(/^SN:.*/, "SN:"SNID, $2)) print }' | samtools reheader -P - "$bam" > "${bam%_sorted.bam}_reheadered.bam"
 
+# Now index the reheadered BAM file
+samtools index "${bam%_sorted.bam}_reheadered.bam"
 
 # Clean up temporary files
 if [ -f "${bam}_sorted.bam" ]; then
