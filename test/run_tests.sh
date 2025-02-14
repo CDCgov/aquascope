f96a7b8c80f483735433b9168dac3fdb3467677d
slsevilla <xuz1@biolinux.biotech.cdc.gov>
2025-02-14 00:43:58 -0500

    test: update test; remove singularity call

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,12 +4,11 @@ mkdir -p $outDir
 
 module purge
 module --ignore-cache load nextflow/24.10.4
-module --ignore-cache load singularity/4.1.4
 
 source /etc/profile
 
 nextflow run main.nf --resume -profile singularity,test_illumina -entry AQUASCOPE --outdir $outDir/ILLUMINA/complete
 nextflow run main.nf --resume -profile singularity,test_illumina -entry QUALITY_ALIGN --outdir $outDir/ILLUMINA/quality
-nextflow run main.nf --resume -profile singularity,test_bam -entry AQUASCOPE --outdir $outDir/BAM/COMPLETE
+nextflow run main.nf --resume -profile singularity,test_bam -entry FREYJA_ONLY --outdir $outDir/BAM/COMPLETE
 nextflow run main.nf --resume -profile singularity,test_iontorrent -entry AQUASCOPE --outdir $outDir/iontorrent
 nextflow run main.nf --resume -profile singularity,test_ont -entry AQUASCOPE --outdir $outDir/ONT/COMPLETE
